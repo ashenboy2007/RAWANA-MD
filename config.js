23e365d28558702e48483a5e30be5f59f26289bb
ashenboy2007 <hhdinuka99@gmail.com>
2025-07-15 06:55:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "ENTER YOUR SESSION ID":process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "ENTER YOUR MONGO DB URL",
+SESSION_ID: process.env.SESSION_ID === undefined? "57EXTYZL#Ri13LAizJE5KkyLrrlx4upl3l6X-BA7Z1jHVRGpuzSE":process.env.SESSION_ID,
+MONGODB: process.env.MONGODB || "mongodb://mongo:xmuhGbIipdGlXbgQlUsVVVOuPIRANTnC@ballast.proxy.rlwy.net:29423",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
 repo: process.env.GITHUB_REPO || "RAWANA-MD",
